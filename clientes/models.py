07e2bcb219d417f1376c465f5502c0f78114f7f1
Edin Botzoc <edinpaau466@gmail.com>
2026-03-19 16:00:55 -0600

    arreglo audiencias

--- clientes/models.py
+++ clientes/models.py
@@ -2,7 +2,23 @@ from django.db import models
 
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
-    direccion = models.TextField()
+    direccion = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.nombre
+
+
+class Audiencia(models.Model):
+    descripcion = models.CharField(max_length=200)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return self.descripcion
+
+
+class Recordatorio(models.Model):
+    descripcion = models.CharField(max_length=200)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return self.descripcion
--- clientes/views.py
+++ clientes/views.py
@@ -2,22 +2,11 @@ from django.shortcuts import render, redirect
 from .models import Cliente
 
 def clientes(request):
-    if request.method == 'POST':
-        nombre = request.POST.get('nombre')
-        direccion = request.POST.get('direccion')
-
-        Cliente.objects.create(
-            nombre=nombre,
-            direccion=direccion
-        )
-
-        return redirect('clientes')
-
     lista = Cliente.objects.all()
     return render(request, 'clientes.html', {'clientes': lista})
 
+def audiencias(request):
+    return render(request, 'audiencias.html')
 
-def eliminar_cliente(request, id):
-    cliente = Cliente.objects.get(id=id)
-    cliente.delete()
-    return redirect('clientes')
+def recordatorios(request):
+    return render(request, 'recordatorios.html')
