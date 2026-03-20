6d48631dcf7f1989cdc77f1295c9d780930a25b4
Edin Botzoc <edinpaau466@gmail.com>
2026-03-19 23:11:38 -0600

    fix inicio root

--- Botzoc_system/urls.py
+++ Botzoc_system/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def inicio(request):
+    return redirect('/clientes/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', inicio),
     path('', include('clientes.urls')),
 ]
