7f3a23c9772e87304a8c85c134c4b5f36b5d0179
Edin Botzoc <edinpaau466@gmail.com>
2026-03-19 18:54:40 -0600

    subiendo migraciones clientes

--- clientes/niews.py
+++ clientes/niews.py
@@ -6,12 +6,4 @@ urlpatterns = [
     path('clientes/', clientes_view),
     path('audiencias/', audiencias_view),
     path('recordatorios/', recordatorios_view),
-]from django.urls import path
-from .views import home, clientes_view, audiencias_view, recordatorios_view
-
-urlpatterns = [
-    path('', home),
-    path('clientes/', clientes_view),
-    path('audiencias/', audiencias_view),
-    path('recordatorios/', recordatorios_view),
 ]
