29b2d042c2509fb162e9b2fb704170677a64c05e
Edin Botzoc <edinpaau466@gmail.com>
2026-03-21 08:36:56 -0600

    fix urls final

--- Botzoc_system/urls.py
+++ Botzoc_system/urls.py
@@ -2,14 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 
-# REDIRECCIÓN AUTOMÁTICA AL SISTEMA
 def inicio(request):
     return redirect('/clientes/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # Página principal → redirige al sistema
+    # Redirección inicial
     path('', inicio),
 
     # Sistema principal
