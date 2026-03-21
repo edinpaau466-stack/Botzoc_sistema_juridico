7618e6aa24093207230e6d4a59843edf001fd259
Edin Botzoc <edinpaau466@gmail.com>
2026-03-21 08:11:09 -0600

    urls corregidas funcionando

--- Botzoc_system/urls.py
+++ Botzoc_system/urls.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 
+# REDIRECCIÓN AUTOMÁTICA AL SISTEMA
 def inicio(request):
     return redirect('/clientes/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', inicio),  # redirige al inicio
+
+    # Página principal → redirige al sistema
+    path('', inicio),
+
+    # Sistema principal
     path('clientes/', include('clientes.urls')),
 ]
