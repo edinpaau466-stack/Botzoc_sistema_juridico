c2cc3f1b25e351e59890b8dbd14342ba7b56bc4f
Edin Botzoc <edinpaau466@gmail.com>
2026-03-21 09:38:28 -0600

    FIX REAL _file_

--- Botzoc_system/settings.py
+++ Botzoc_system/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-BASE_DIR = Path(_file_).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-cambia-esto-en-produccion'
 
